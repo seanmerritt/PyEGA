ab8b1cdbb845e8acbc8034b5195def798eeb9b90
seanmerritt <seanmerritt10@gmail.com>
2022-07-12 11:56:11 -0600

    start files

--- EGA/EGA.py
+++ EGA/EGA.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.covariance import GraphicalLasso, shrunk_covariance
+import igraph
 from igraph import Graph
 import numpy as np
 from sklearn.preprocessing import StandardScaler
@@ -20,7 +21,7 @@ class EGA():
         alphaRange = 10.0 ** np.arange(-8,0) # 1e-7 to 1e-1 by order of magnitude
         matrix = shrunk_covariance(self.cvm,.8)
         glasso_matrix = GraphicalLasso(max_iter = 1000).fit(matrix, 0.1)
-        self.glasso_cvm = glasso_matrix.covariance_
+        self.glasso_cvm =  pd.DataFrame(glasso_matrix.covariance_)
         '''
         for alpha in alphaRange:
             try: 
@@ -30,13 +31,22 @@ class EGA():
                 print("Failed at alpha=%s"%alpha)
         '''
     def cvm_to_graph(self):
-        self.graph = Graph.Adjacency(self.glasso_cvm,attribute="weighted")
+        A = self.glasso_cvm.values
+
+        # Create graph, A.astype(bool).tolist() or (A / A).tolist() can also be used.
+        self.graph = igraph.Graph.Adjacency((A > 0).tolist())
+
+        # Add edge weights and node labels.
+        self.graph.es['weight'] = A[A.nonzero()]
+        self.graph.vs['label'] =  self.glasso_cvm.index #/a.columns
+        
     
     def detect_communities(self):
-        algorithms.walktrap(self.graph)
+        print(self.graph.community_walktrap().as_clustering().membership)
     
     def fit(self, data):
         self.df_to_matrix(data)
         self.to_glasso_cvm()
         self.cvm_to_graph()
-        self.detect_communities()
+        igraph.plot(self.graph)
+        #self.detect_communities()
